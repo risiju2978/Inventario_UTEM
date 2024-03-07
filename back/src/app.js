14500254152513e1d67938f6207f4a4e75e02855
Jose Germán Martínez <jgermanmm@gmail.com>
2024-03-06 23:56:31 -0300

    Actualizar configuración CORS en app.js

--- back/src/app.js
+++ back/src/app.js
@@ -12,21 +12,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Configuración CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, DELETE'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Authorization,X-API-KEY,Origin,X-Requested-With,Content-Type,Accept,Access-Control-Allow-Request-Method'
-  );
-  res.setHeader('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-//app.use(cors);
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, OPTIONS, PUT, DELETE'
+//   );
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Authorization,X-API-KEY,Origin,X-Requested-With,Content-Type,Accept,Access-Control-Allow-Request-Method'
+//   );
+//   res.setHeader('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
+app.use(cors);
 
 
 
