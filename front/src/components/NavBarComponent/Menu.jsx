17531425232cd26427041c240762fb706d959ec8
Jose Germán Martínez <jgermanmm@gmail.com>
2024-03-11 17:44:20 -0300

    Agregado de autenticación y visualización del nombre de usuario en la barra de navegación

--- front/src/components/NavBarComponent/Menu.jsx
+++ front/src/components/NavBarComponent/Menu.jsx
@@ -1,47 +1,58 @@
+import { useEffect, useState } from "react";
+import { useUserContext } from "../../context/UserAppContext";
+import { useAuthContext } from "../../context/AuthContext";
+
 export function MenuComponent() {
+  const { user } = useUserContext();
+  const { isAuthenticated } = useAuthContext();
+  const [usuario, setUsuario] = useState(null);
+
+  useEffect(() => {
+    if (user) setUsuario(user);
+  }, [user]);
+
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary mb-2 justify-content-center">
-  <div className="container-fluid">
-    <span className="navbar-brand" href="#">
-      Inventario UTEM
-    </span>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <button className="nav-link active" aria-current="page">
-            <a href="/login">Login </a>
-          </button>
-        </li>
-        <li className="nav-item">
-          <button className="nav-link">
-            Features
-          </button>
-        </li>
-        <li className="nav-item">
-          <button className="nav-link">
-            Pricing
-          </button>
-        </li>
-        <li className="nav-item">
-          <button className="nav-link disabled" aria-disabled="true">
-            Disabled
-          </button>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
+      <div className="container-fluid">
+        <span className="navbar-brand" href="#">
+          Inventario UTEM
+        </span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            {isAuthenticated && usuario !== null ? (
+              <button className="nav-link active" >Hola, {usuario.username}</button>
+            ) : (
+              <li className="nav-item">
+                <button className="nav-link active" aria-current="page">
+                  <a href="/login">Login </a>
+                </button>
+              </li>
+            )}
+            <li className="nav-item">
+              <button className="nav-link">Features</button>
+            </li>
+            <li className="nav-item">
+              <button className="nav-link">Pricing</button>
+            </li>
+            <li className="nav-item">
+              <button className="nav-link disabled" aria-disabled="true">
+                Disabled
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
