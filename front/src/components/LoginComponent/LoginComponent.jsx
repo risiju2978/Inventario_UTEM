6f70e9606a7062a0942ef252a5fb8fc307f86364
Jose Germán Martínez <jgermanmm@gmail.com>
2024-03-11 15:09:24 -0300

    Añadir funcionalidad de inicio de sesión en LoginComponent.jsx

--- front/src/components/LoginComponent/LoginComponent.jsx
+++ front/src/components/LoginComponent/LoginComponent.jsx
@@ -1,31 +1,87 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../context/AuthContext";
+import { Api } from "../../api/api";
 
 const LoginComponent = () => {
+  const history = useNavigate();
+  const { login } = useAuthContext();
+  const [userDataLogin, setUserDataLogin] = useState({
+    username: "",
+    password: "",
+  });
 
+  const handleInputChange = (event) => {
+    setUserDataLogin({
+      ...userDataLogin,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-    
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (userDataLogin.username === "" || userDataLogin.password === "") {
+      alert("Todos los campos son requeridos");
+      return;
+    }
+
+    Api.login(userDataLogin.username, userDataLogin.password)
+      .then((res) => {
+        console.log(res);
+        if (res.status !== 200) {
+          console.log(res.error);
+          return;
+        }
+        login();
+        history("/");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Error al iniciar sesión", error.message);
+      });
+  };
+
   return (
     <div className="container">
-        <div className="row">
-            <div className="col-md-6 offset-md-3">
-            <h1>Acceder al sistema</h1>
-            <form>
-                <div className="mb-3">
-                <label htmlFor="username-login" className="form-label">Nombre de usuario</label>
-                <input type="username" className="form-control" id="username-login" aria-describedby="emailHelp" />
-                </div>
-                <div className="mb-3">
-                <label htmlFor="username-login" className="form-label">Contraseña</label>
-                <input type="password" className="form-control" id="password-login" />
-                </div>
-                <div className="mb-3 form-check">
-                </div>
-                <button type="submit" className="btn btn-primary">Ingresar</button>
-            </form>
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h1>Acceder al sistema</h1>
+          <form>
+            <div className="mb-3">
+              <label htmlFor="username-login" className="form-label">
+                Nombre de usuario
+              </label>
+              <input
+                type="username"
+                className="form-control"
+                id="username-login"
+                aria-describedby="emailHelp"
+                onChange={handleInputChange}
+                name="username"
+              />
             </div>
+            <div className="mb-3">
+              <label htmlFor="username-login" className="form-label">
+                Contraseña
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password-login"
+                onChange={handleInputChange}
+                name="password"
+              />
+            </div>
+            <div className="mb-3 form-check"></div>
+            <button type="submit" className="btn btn-primary" onClick={handleSubmit}>
+              Ingresar
+            </button>
+          </form>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginComponent
+export default LoginComponent;
