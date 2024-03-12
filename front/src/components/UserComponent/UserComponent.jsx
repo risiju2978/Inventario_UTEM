094c41c4abc288342af145a974f7deecba6f28a8
Jose Germán Martínez <jgermanmm@gmail.com>
2024-03-12 14:54:06 -0300

    Agregado botón de visualización en UserComponent.jsx

--- front/src/components/UserComponent/UserComponent.jsx
+++ front/src/components/UserComponent/UserComponent.jsx
@@ -90,6 +90,9 @@ function UserComponent() {
                     <button type="button" className="btn btn-danger mx-2">
                     <i className="bi bi-file-earmark-x"></i>
                     </button>
+                    <button type="button" className="btn btn-warning">
+                    <i className="bi bi-eye"></i>
+                    </button>
                     </div>
                   </td>
                 </tr>
